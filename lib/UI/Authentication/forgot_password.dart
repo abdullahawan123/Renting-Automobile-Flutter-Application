73af9212554dc8da1acfd318d52e9cbb330631bd
Abdullah Awan <awana2202@gmail.com>
2023-03-12 11:12:54 -0700

    forgot password module part 2

--- lib/UI/Authentication/forgot_password.dart
+++ lib/UI/Authentication/forgot_password.dart
@@ -24,7 +24,6 @@ class _ForgetPasswordState extends State<ForgetPassword> {
 
   @override
   void dispose() {
-    // TODO: implement dispose
     super.dispose();
     emailController.dispose();
   }
