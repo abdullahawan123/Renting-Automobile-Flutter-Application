3b43c3594dd4ea53a115667faaf637bd84a9ba5b
Abdullah Awan <awana2202@gmail.com>
2023-05-27 12:33:29 -0700

    Business Owner homepage and details page is almost complete.

--- lib/UI/BO_Screens/BO_Details.dart
+++ lib/UI/BO_Screens/BO_Details.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:firebase_storage/firebase_storage.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 import 'package:wheel_for_a_while/UI/utils/utilities.dart';
 
 class BO_Details extends StatefulWidget {
@@ -18,13 +20,17 @@ class _BO_DetailsState extends State<BO_Details> {
   final TextEditingController _capacityController = TextEditingController();
   final TextEditingController _gearsController = TextEditingController();
   final TextEditingController _acController = TextEditingController();
+  final TextEditingController _dailyPriceController = TextEditingController();
+  final TextEditingController _monthlyPriceController = TextEditingController();
+
+  FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
 
   final ImagePicker _picker = ImagePicker();
   final List<XFile> _selectedList = [];
-  final FirebaseStorage _storageRef = FirebaseStorage.instance;
-  List<String> arrImgUrl = [];
-  int uploadItem = 0;
-  bool _isUploading = false;
+  bool loading = false ;
+
 
   @override
   Widget build(BuildContext context) {
@@ -36,7 +42,7 @@ class _BO_DetailsState extends State<BO_Details> {
         child: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.all(16.0),
-            child: _isUploading? isUploading() :Column(
+            child: Column(
               children: [
                 TextField(
                   controller: _nameController,
@@ -76,7 +82,23 @@ class _BO_DetailsState extends State<BO_Details> {
                   decoration: const InputDecoration(labelText: 'Automatic or Manual', border: OutlineInputBorder()),
                 ),
                 const SizedBox(height: 10),
-
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        controller: _dailyPriceController,
+                        decoration: const InputDecoration(labelText: 'Price', hintText: 'Daily', border: OutlineInputBorder()),
+                      ),
+                    ),
+                    const SizedBox(width: 5,),
+                    Expanded(
+                      child: TextField(
+                        controller: _monthlyPriceController,
+                        decoration: const InputDecoration(labelText: 'Price', hintText: 'Monthly', border: OutlineInputBorder()),
+                      ),
+                    ),
+                  ],
+                ),
                 const SizedBox(height: 10),
                 TextField(
                   controller: _acController,
@@ -117,67 +139,137 @@ class _BO_DetailsState extends State<BO_Details> {
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           if(_selectedList.isNotEmpty){
-            uploadFunction(_selectedList);  
+            if(_nameController.text.isEmpty ||
+                _categoryController.text.isEmpty ||
+                _modelController.text.isEmpty ||
+                _capacityController.text.isEmpty ||
+                _makeController.text.isEmpty ||
+                _acController.text.isEmpty ||
+                _dailyPriceController.text.isEmpty ||
+                _monthlyPriceController.text.isEmpty){
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: const Text('Error'),
+                    content: const Text('Please fill in all fields'),
+                    actions: [
+                      TextButton(
+                        child: const Text('OK'),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                    ],
+                  );
+                },
+              );
+            }
+            else{
+              setState(() {
+                loading = true ;
+              });
+              uploadFunction(_selectedList);
+            }
+
           }else{
-            ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Please Select Image First??')));
+            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Please Select at least one image?')));
           }
           
         },
         tooltip: 'Upload',
-        child: const Icon(Icons.file_upload),
+        child: loading
+            ? const CircularProgressIndicator(strokeWidth: 4, color: Color(0xFF03DAC6),)
+            : const Icon(Icons.file_upload),
       ),
     );
   }
 
-  void uploadFunction(List<XFile> images){
-    setState(() {
-      _isUploading = true;
-    });
-    for(int i = 0; i < images.length; i++){
-      var imageUrl = uploadFile(images[i]);
-      arrImgUrl.add(imageUrl.toString());
+  void uploadFunction(List<XFile> images) async {
+    List<String> imageUrls = [];
+    for (int i = 0; i < images.length; i++) {
+      String imageUrl = await uploadFile(images[i]);
+      if (imageUrl.isNotEmpty) {
+        imageUrls.add(imageUrl);
+      }
+    }
+    if (imageUrls.isNotEmpty) {
+      saveDataToFirestore(imageUrls);
     }
   }
 
-  Future<String> uploadFile(XFile image)async{
-    Reference reference = _storageRef.ref().child('multiple_images').child(image.name);
-    UploadTask uploadTask = reference.putFile(File(image.path));
-    await uploadTask.whenComplete(() {
-      setState(() {
-        uploadItem++;
-        if(uploadItem == _selectedList.length){
-          _isUploading = false;
-          uploadItem = 0;
-        }
-      });
-    });
-    return reference.getDownloadURL();
+  Future<String> uploadFile(XFile image) async {
+    firebase_storage.Reference reference =
+    firebase_storage.FirebaseStorage.instance.ref('/automobiles_images/'+DateTime.now().millisecondsSinceEpoch.toString());
+    firebase_storage.UploadTask uploadTask = reference.putFile(File(image.path));
+
+    try {
+      await uploadTask;
+      String downloadUrl = await reference.getDownloadURL();
+      return downloadUrl;
+    } catch (e) {
+      Utils().toastMessage("Error While Uploading ${e.toString()}");
+      return e.toString();
+    }
   }
 
-  Future<void> selectImage()async{
+  void saveDataToFirestore(List<String> imageUrls) {
+    var user = _auth.currentUser;
+    Map<String, dynamic> data = {
+      'user_id' : user?.uid,
+      'automobile_name': _nameController.text,
+      'category': _categoryController.text,
+      'model': _modelController.text,
+      'make': _makeController.text,
+      'capacity': _capacityController.text,
+      'no_of_gear': _gearsController.text,
+      'ac or non-ac': _acController.text,
+      'daily_price': _dailyPriceController.text,
+      'monthly_price': _monthlyPriceController.text,
+      'image_URL': imageUrls,
+    };
+
+    var id = "${DateTime.now().millisecondsSinceEpoch}";
+
+    FirebaseFirestore.instance
+        .collection('automobile')
+        .doc(id)
+        .set(data)
+        .then((_) {
+          setState(() {
+            loading = false ;
+          });
+          Navigator.pop(context);
+    }).catchError((error) {
+      setState(() {
+        loading = false;
+      });
+      Utils().toastMessage("Error While Uploading ${error.toString()}");
+    });
+  }
+
+  Future<void> selectImage() async {
     _selectedList.clear();
-    try{
+    try {
       final List<XFile> imgs = await _picker.pickMultiImage();
-      if(imgs.isNotEmpty){
+      if (imgs.isNotEmpty) {
         _selectedList.addAll(imgs);
       }
-    }catch(e){
+    } catch (e) {
       Utils().toastMessage(e.toString());
     }
     setState(() {});
   }
 
-  Widget isUploading() {
-    return Center(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text("Uploading: $uploadItem/${_selectedList.length}"),
-          const SizedBox(height: 10,),
-          const CircularProgressIndicator(strokeWidth: 4, color: Color(0xFF03DAC6),)
-        ],
-      ),
-    );
-  }
+  // Widget isUploading() {
+  //   return Column(
+  //     crossAxisAlignment: CrossAxisAlignment.center,
+  //     mainAxisAlignment: MainAxisAlignment.center,
+  //     children: [
+  //       Center(child: Text("Uploading: $uploadItem/${_selectedList.length}")),
+  //       const SizedBox(height: 10,),
+  //       const Center(child: CircularProgressIndicator(strokeWidth: 4, color: Color(0xFF03DAC6),))
+  //     ],
+  //   );
+  // }
 }
--- lib/UI/BO_Screens/BO_HomePage.dart
+++ lib/UI/BO_Screens/BO_HomePage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:wheel_for_a_while/UI/Authentication/Login.dart';
 import 'package:wheel_for_a_while/UI/BO_Screens/BO_Details.dart';
 import 'package:wheel_for_a_while/UI/utils/BackButton.dart';
@@ -8,74 +10,103 @@ class BO_HomePage extends StatefulWidget {
   _BO_HomePageState createState() => _BO_HomePageState();
 }
 
-class _BO_HomePageState extends State<BO_HomePage> with SingleTickerProviderStateMixin {
-  late AnimationController _animationController;
-  late Animation<double> _animation;
+class _BO_HomePageState extends State<BO_HomePage> {
+  late String currentUserUid = '';
 
   @override
   void initState() {
     super.initState();
-    // Create animation controller
-    _animationController = AnimationController(
-      duration: const Duration(seconds: 2),
-      vsync: this,
-    );
-
-    // Create animation
-    _animation = Tween<double>(begin: 0, end: 1).animate(_animationController);
-
-    // Start the animation
-    _animationController.forward();
+    getUserUid();
   }
 
-  @override
-  void dispose() {
-    _animationController.dispose();
-    super.dispose();
+  Future<void> getUserUid() async {
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      setState(() {
+        currentUserUid = user.uid;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
-      onWillPop: ()=> BackButtonDoublePressed().onBackButtonDoublePressed(context),
+      onWillPop: () => BackButtonDoublePressed().onBackButtonDoublePressed(context),
       child: Scaffold(
         appBar: AppBar(
-          automaticallyImplyLeading: false,
           title: const Text('Business Owner'),
-          actions: [
-            IconButton(onPressed: (){
+          centerTitle: true,
+          automaticallyImplyLeading: false,
+          leading: IconButton(
+            icon: const Icon(Icons.logout_outlined),
+            onPressed: (){
               Navigator.push(context, MaterialPageRoute(builder: (context) => const Login()));
-            }, icon: const Icon(Icons.logout))
-          ],
+            }
+          ),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            FadeTransition(
-              opacity: _animation,
-              child: const Text(
-                'No automobile details found',
-                style: TextStyle(fontSize: 20),
-              ),
-            ),
-            const SizedBox(height: 20),
-            Align(
-              alignment: Alignment.bottomRight,
-              child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: FloatingActionButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => BO_Details()),
+        body: SafeArea(
+          child: StreamBuilder<QuerySnapshot>(
+            stream: FirebaseFirestore.instance
+                .collection('automobile')
+                .where('user_id', isEqualTo: currentUserUid)
+                .snapshots(),
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return Center(
+                  child: Text('Error: ${snapshot.error}'),
+                );
+              }
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+              if (snapshot.hasData && snapshot.data!.docs.isNotEmpty) {
+                // Automobiles exist, display the details
+                return ListView.builder(
+                  itemCount: snapshot.data!.docs.length,
+                  itemBuilder: (context, index) {
+                    final automobile = snapshot.data!.docs[index];
+                    final imageUrls =
+                    List<String>.from(automobile['image_URL']);
+                    final name = automobile['automobile_name'] ?? '';
+                    final make = automobile['make'] ?? '';
+                    final model = automobile['model'] ?? '';
+
+                    return ListTile(
+                      leading: Image.network(imageUrls.first),
+                      title: Text(name),
+                      subtitle: Text('$make $model'),
                     );
                   },
-                  child: const Icon(Icons.arrow_forward),
+                );
+              }
+
+              return Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Text('No automobiles found.', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, fontFamily: 'ShantellSans'),),
+                      Text('If you want to add an automobile, ', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, fontFamily: 'ShantellSans'),),
+                      Text('pressed the button below', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, fontFamily: 'ShantellSans'),),
+                      Text('At the right corner!!!!!', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, fontFamily: 'ShantellSans'),)
+                    ],
+                  ),
                 ),
-              ),
-            ),
-          ],
+              );
+            },
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => BO_Details()),
+            );
+          },
+          child: const Icon(Icons.add),
         ),
       ),
     );
